5891fb524e998d386e3a276f12c7510203dd3ed5
DavidEgiazaryan <166252012+DavidEgiazaryan@users.noreply.github.com>
2025-09-20 13:22:49 +0300

    Changed addition on subtraction

--- calculator.py
+++ calculator.py
@@ -3,6 +3,6 @@ print("Простой калькулятор")
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 
-res = num1 + num2
+res = num1 - num2
 
 print(f"Сумма чисел равна: {res}")
